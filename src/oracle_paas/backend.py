24e815c4fcd2a941fa89b7a148f29fc81a258432
Roman Kosenko <Roman.Kosenko@gmail.com>
2016-04-25 16:01:21 +0000

    Complete Oracle management
    - NC-990

--- src/oracle_paas/backend.py
+++ src/oracle_paas/backend.py
@@ -1,13 +1,97 @@
-from nodeconductor.structure import ServiceBackend
+from django.conf import settings as django_settings
+from rest_framework.reverse import reverse
+
+from nodeconductor.core.utils import request_api
+from nodeconductor.structure import ServiceBackend, ServiceBackendError
+from nodeconductor_jira.models import Project, Issue
+
+
+class OracleBackendError(ServiceBackendError):
+    pass
 
 
 class OracleBackend(ServiceBackend):
 
     def __init__(self, settings):
         self.settings = settings
+        self.templates = getattr(django_settings, 'ORACLE_TICKET_TEMPLATES', {})
+
+        if not self.templates or 'provision' not in self.templates:
+            raise OracleBackendError(
+                "Improperly configured: ORACLE_TICKET_TEMPLATES should be defined")
 
     def ping(self, raise_exception=False):
         return True
 
     def sync(self):
         pass
+
+    def provision(self, deployment, request, ssh_key=None):
+        try:
+            proj = Project.objects.get(backend_id=self.settings.token, available_for_all=True)
+        except Project.DoesNotExist:
+            self._set_erred(deployment, "Can't find JIRA project '%s'" % self.settings.token)
+
+        # create fake and empty SshKey instance for string formating
+        if not ssh_key:
+            ssh_key = type('SshKey', (object,), {'name': '', 'uuid': type('UUID', (object,), {'hex': ''})})
+
+        message = self._compile_message(deployment, 'provision', ssh_key=ssh_key)
+        payload = {
+            "project": reverse('jira-projects-detail', kwargs={'uuid': proj.uuid.hex}),
+            "summary": self.templates['provision']['summary'],
+            "description": message,
+            "priority": dict(Issue.Priority.CHOICES).get(Issue.Priority.MINOR),
+            "impact": dict(Issue.Impact.CHOICES).get(Issue.Impact.MEDIUM),
+        }
+
+        response = request_api(request, 'jira-issues-list', method='POST', data=payload)
+        if not response.success:
+            self._set_erred(deployment, "Can't create JIRA ticket: %s" % response.status)
+
+        deployment.support_request = Issue.objects.get(uuid=response.data['uuid'])
+        deployment.begin_provisioning()
+        deployment.save(update_fields=['state'])
+
+    def destroy(self, deployment, request):
+        payload = {
+            "issue": reverse('jira-issues-detail', kwargs={'uuid': deployment.support_request.uuid.hex}),
+            "message": self._compile_message(deployment, 'undeploy'),
+        }
+        response = request_api(request, 'jira-comments-list', method='POST', data=payload)
+        if not response.success:
+            self._set_erred(deployment, "Can't add JIRA comment: %s" % response.status)
+
+        deployment.begin_deleting()
+        deployment.save(update_fields=['state'])
+
+    def resize(self, deployment, request):
+        payload = {
+            "issue": reverse('jira-issues-detail', kwargs={'uuid': deployment.support_request.uuid.hex}),
+            "message": self._compile_message(deployment, 'resize'),
+        }
+        response = request_api(request, 'jira-comments-list', method='POST', data=payload)
+        if not response.success:
+            self._set_erred(deployment, "Can't add JIRA comment: %s" % response.status)
+
+        deployment.begin_resizing()
+        deployment.save(update_fields=['state'])
+
+    def _compile_message(self, deployment, template_name, add_title=False, **kwargs):
+        template = self.templates[template_name]
+        message = unicode(template['details']).format(
+            deployment=deployment,
+            customer=deployment.service_project_link.service.customer,
+            project=deployment.service_project_link.project,
+            **kwargs)
+
+        if add_title:
+            message = template['summary'] + '\n\n' + message
+
+        return message
+
+    def _set_erred(self, deployment, message=''):
+        deployment.error_message = message
+        deployment.set_erred()
+        deployment.save(update_fields=['state', 'error_message'])
+        raise OracleBackendError(deployment.error_message)
--- src/oracle_paas/views.py
+++ src/oracle_paas/views.py
@@ -1,9 +1,10 @@
 from django.utils import timezone
-from rest_framework import decorators, response, status
+from rest_framework import decorators, exceptions, response, status
 
 from nodeconductor.structure import views as structure_views
 
 from . import models, serializers
+from .backend import OracleBackendError
 
 
 class OracleServiceViewSet(structure_views.BaseServiceViewSet):
@@ -20,13 +21,19 @@ class DeploymentViewSet(structure_views.BaseResourceViewSet):
     queryset = models.Deployment.objects.all()
     serializer_class = serializers.DeploymentSerializer
 
+    def get_serializer_class(self):
+        if self.action == 'resize':
+            return serializers.DeploymentResizeSerializer
+        return super(DeploymentViewSet, self).get_serializer_class()
+
     def perform_provision(self, serializer):
         resource = serializer.save()
-        resource.begin_provisioning()
-        resource.save()
+        backend = resource.get_backend()
+        backend.provision(resource, self.request, ssh_key=serializer.validated_data.get('ssh_public_key'))
 
     @decorators.detail_route(methods=['post'])
-    def provision(self, request, uuid=None):
+    @structure_views.safe_operation(valid_state=models.Deployment.States.PROVISIONING)
+    def provision(self, request, resource, uuid=None):
         """ Complete provisioning. Example:
 
             .. code-block:: http
@@ -47,7 +54,69 @@ class DeploymentViewSet(structure_views.BaseResourceViewSet):
             resource.report = report
             resource.start_time = timezone.now()
             resource.set_online()
-            resource.save()
+            resource.save(update_fields=['state'])
             return response.Response({'detail': "Provision complete"}, status=status.HTTP_200_OK)
         else:
             return response.Response({'detail': "Empty report"}, status=status.HTTP_400_BAD_REQUEST)
+
+    @decorators.detail_route(methods=['post'])
+    # @structure_views.safe_operation(valid_state=(models.Deployment.States.ONLINE, models.Deployment.States.RESIZING))
+    @structure_views.safe_operation()
+    def resize(self, request, resource, uuid=None):
+        """ Request for DB Instance resize. Example:
+
+            .. code-block:: http
+
+                POST /api/oracle-deployments/a04a26e46def4724a0841abcb81926ac/resize/ HTTP/1.1
+                Content-Type: application/json
+                Accept: application/json
+                Authorization: Token c84d653b9ec92c6cbac41c706593e66f567a7fa4
+                Host: example.com
+
+                {
+                    "flavor": "http://example.com/api/openstack-flavors/ef86802458684056b18576a91daf7690/"
+                }
+
+            To confirm resize complete issue an empty post to the same endpoint.
+        """
+
+        if resource.state == resource.States.RESIZING:
+            resource.set_resized()
+            resource.save(update_fields=['state'])
+            return response.Response({'detail': "Resizing complete"}, status=status.HTTP_200_OK)
+
+        serializer = self.get_serializer(resource, data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        resource.state = resource.States.RESIZING_SCHEDULED
+        resource.flavor = serializer.validated_data.get('flavor')
+        resource.save(update_fields=['flavor', 'state'])
+
+        try:
+            backend = resource.get_backend()
+            backend.resize(resource, self.request)
+        except OracleBackendError as e:
+            raise exceptions.APIException(e)
+
+        return response.Response({'detail': "Resizing scheduled"}, status=status.HTTP_200_OK)
+
+    @structure_views.safe_operation(valid_state=(models.Deployment.States.ONLINE, models.Deployment.States.DELETING))
+    def destroy(self, request, resource, uuid=None):
+        """ Request for DB Instance deletion or confirm deletion success.
+            A proper action will be taken depending on the current deployment state.
+        """
+
+        if resource.state == resource.States.DELETING:
+            self.perform_destroy(resource)
+            return response.Response({'detail': "Deployment deleted"}, status=status.HTTP_200_OK)
+
+        resource.state = resource.States.DELETION_SCHEDULED
+        resource.save(update_fields=['state'])
+
+        try:
+            backend = resource.get_backend()
+            backend.destroy(resource, self.request)
+        except OracleBackendError as e:
+            raise exceptions.APIException(e)
+
+        return response.Response({'detail': "Deletion scheduled"}, status=status.HTTP_200_OK)
