ad813973421ed828f724a999fabbc12c4e429247
Ilja Livenson <ilja@opennodecloud.com>
2016-05-16 13:10:47 +0400

    Use generic state filter instead of custom one
    - ITACLOUD-6837

--- src/nodeconductor_paas_oracle/filters.py
+++ src/nodeconductor_paas_oracle/filters.py
@@ -1,13 +1,13 @@
 import django_filters
 
+from nodeconductor.structure.filters import BaseResourceStateFilter
 from .models import Deployment
 
 
-class DeploymentFilter(django_filters.FilterSet):
+class DeploymentFilter(BaseResourceStateFilter):
     db_name = django_filters.CharFilter()
-    state = django_filters.CharFilter()
 
-    class Meta(object):
+    class Meta(BaseResourceStateFilter.Meta):
         model = Deployment
         fields = [
             'db_name',
